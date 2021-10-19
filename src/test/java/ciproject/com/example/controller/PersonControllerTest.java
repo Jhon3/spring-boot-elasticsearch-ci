f5a6b7d777764c1a79d84c7a337d53e90d4fdb51
jhon3 <cabral.jhon00@gmail.com>
2021-10-19 18:00:08 -0300

    exemplo de falha

--- src/test/java/ciproject/com/example/controller/PersonControllerTest.java
+++ src/test/java/ciproject/com/example/controller/PersonControllerTest.java
@@ -48,7 +48,7 @@ public class PersonControllerTest {
     }
     @Test
     public void test03() {
-        System.out.println("Test");
+        System.out.println("Test vai falhar!");
         assertThat(true).isEqualTo(false);
     }
 }
